5544b41b9efdc493c0b532202babec9755d58745
DHARSHAN-sketch <dharshans640@gmail.com>
2025-11-17 13:26:42 +0530

    Enhance spi_transaction class with new data members
    Added new data members for master and slave communication and updated display function.

--- SPI_SV_TESTBENCH/Testbench/spi_transactions.sv
+++ SPI_SV_TESTBENCH/Testbench/spi_transactions.sv
@@ -1,21 +1,27 @@
-class spi_generator;
+class spi_transaction;
 
-  mailbox #(spi_transaction) gen2drv_mb;
-  int repeat_count = 5;
-  bit verbose = 1;
+  // master->slave
+  rand bit [7:0] m_data_in;   // master transmit data
+  // slave->master
+  rand bit [7:0] s_data_in;   // slave  transmit data   <-- added
 
-  function new(mailbox #(spi_transaction) mb);
-    this.gen2drv_mb = mb;
+  // Observed
+  bit [7:0]      s_data_out;  // slave received data
+  bit [7:0]      m_data_out;  // master received data  <-- added
+
+  constraint data_range {
+    m_data_in inside {[0:255]};
+    s_data_in inside {[0:255]};  // <-- added
+  }
+
+  function void display(string tag = "");
+    $display("-----------------------------------");
+    $display(" - [%s]", tag);
+    $display("m_data_in  = %0d", m_data_in);
+    $display("s_data_in  = %0d", s_data_in);   // <-- added
+    $display("m_data_out = %0d", m_data_out); // <-- added
+    $display("s_data_out = %0d", s_data_out);
+    $display("-----------------------------------");
   endfunction
-
-  task run();
-    spi_transaction t;
-    repeat (repeat_count) begin
-      t = new();
-      assert(t.randomize());
-      if (verbose) t.display("generator");
-      gen2drv_mb.put(t);
-    end
-  endtask
 
 endclass
